09faa15ef60b5017db1b7c96d934e3f99bb7e350
DerangedDeceiver <derangeddeceiver@gmail.com>
2022-02-06 12:28:35 -0500

    Add optimization test for minkowski p value

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ print(lookup_category)
 from sklearn.neighbors import KNeighborsClassifier
 
 # create a 3NN classifier for our data
-knn = KNeighborsClassifier(n_neighbors=3)
+knn = KNeighborsClassifier(n_neighbors=4, p=2)
 X = data[[
     'pelvic_incidence',
     'pelvic_tilt',
@@ -44,7 +44,7 @@ print(knn.predict_proba(unknown1))
 from sklearn.model_selection import train_test_split
 #random_state: set seed for random# generator
 #test_size: default 25% testing, 75% training
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.20, random_state=42)
 
 # Train the classifier (fit the estimator) using the training data
 knn.fit(X_train, y_train)
@@ -54,11 +54,19 @@ knn.score(X_test, y_test)
 
 # How sensitive is k-NN classification accuracy to the choice of the 'k' parameter?
 k_range = range(1, 20)
-scores = []
+kScores = []
 for k in k_range:
-    knn = KNeighborsClassifier(n_neighbors = k)
+    knn = KNeighborsClassifier(n_neighbors=k, p=2)
     knn.fit(X_train, y_train)
-    scores.append(knn.score(X_test, y_test))
+    kScores.append(knn.score(X_test, y_test))
+
+# What 'p' value (degree of minkowski distance) results in the highest accuracy?
+p_range = range(1, 8)
+pScores = []
+for pVal in p_range:
+    knn = KNeighborsClassifier(n_neighbors=4, p=pVal)
+    knn.fit(X_train, y_train)
+    pScores.append(knn.score(X_test, y_test))
 
 # visualization
 # plotting a scatter matrix
@@ -80,13 +88,19 @@ ax.set_zlabel('pelvic_radius')
 plt.figure()
 plt.xlabel('k')
 plt.ylabel('accuracy')
-plt.scatter(k_range, scores)
+plt.scatter(k_range, kScores)
 plt.xticks([0, 5, 10, 15, 20])
+
+plt.figure()
+plt.xlabel('p')
+plt.ylabel('accuracy')
+plt.scatter(p_range, pScores)
+plt.xticks([0, 2, 4, 6, 8])
 
 # How sensitive is k-NN classification accuracy to the train/test split proportion?
 import numpy as np
 t = [0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2]
-knn = KNeighborsClassifier(n_neighbors=5)
+knn = KNeighborsClassifier(n_neighbors=4, p=2)
 plt.figure()
 for s in t:
     scores = []
